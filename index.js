98d63ef9fcd886bb539034d712b1dfef046494b1
justynchen <justynchen@tencent.com>
2020-08-12 10:39:22 +0800

    fix: do not print the callback param

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
 	init: function(){
 		if(process.env && process.env.SCF_FUNCTIONNAME){
 			process.stderr.write = console._stderr.write = console._stdout.write = process.stdout.write = function () {
-				console.log(...arguments)
+				console.log(arguments[0])
 			}
 		}else{
 			console.log('WARNING: PLEASE USE THIS MODULE IN SCF ONLY')
